a4eaecbd791f69ccd19b6b0b747b51c1719b7893
BayMichael <yoyomike13@gmail.com>
2022-09-27 19:02:18 +0200

    Added deck, playerHand and Dealerhand lists

---
+++ blackjack.py
@@ -0,0 +1,8 @@
+# Importing modules
+import random
+
+# Deck of cards / player and dealer hand
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        "J", "Q", "K", "A", "J", "Q", "K", "A", "J", "Q", "K", "A", "J", "Q", "K", "A",]
+playerHand = []
+dealerHand = []
